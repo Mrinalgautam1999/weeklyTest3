52630d26af52142cf73f91cd4aa606c5170cfd08
Mrinal Gautam <mmrinalgautam1999@gmail.com>
2023-10-19 16:29:35 +0530

    assignment

--- Restaurant-App/src/main/java/com/geekster/Restaurant/App/HotelManagementApp_Application.java
+++ Restaurant-App/src/main/java/com/geekster/Restaurant/App/HotelManagementApp_Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantApp_Application {
+public class HotelManagementApp_Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantApp_Application.class, args);
+		SpringApplication.run(HotelManagementApp_Application.class, args);
 	}
 
 }
